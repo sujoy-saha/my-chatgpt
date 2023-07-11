cfc7f92703cea6f99cf6310e9a9a7f02e6779492
sujoykumarsaha <sujoykumarsaha@gmail.com>
2023-07-11 22:41:40 +0200

    Add more authentication providers

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,23 +1,56 @@
 import NextAuth from 'next-auth'
 import Okta from 'next-auth/providers/okta'
+import Auth0 from "next-auth/providers/auth0"
+import Facebook from "next-auth/providers/facebook"
+import Github from "next-auth/providers/github"
+import Google from "next-auth/providers/google"
+import Twitter from "next-auth/providers/twitter"
+import LinkedIn from "next-auth/providers/linkedin"
 
 export const authOptions = {
     // Configure one or more authentication providers
     providers: [
         Okta({
             idToken: true,
-            clientId: process.env.OKTA_OAUTH2_CLIENT_ID as string,
-            clientSecret: process.env.OKTA_OAUTH2_CLIENT_SECRET as string,
-            issuer: process.env.OKTA_OAUTH2_ISSUER as string,
+            clientId: process.env.OKTA_CLIENT_ID as string,
+            clientSecret: process.env.OKTA_CLIENT_SECRET as string,
+            issuer: process.env.OKTA_ISSUER as string,
             authorization: {
                 params: {
                     scope: "openid email profile offline_access"
                 }
             }
         }),
+        Auth0({
+            clientId: process.env.AUTH0_CLIENT_ID as string, 
+            clientSecret: process.env.AUTH0_CLIENT_SECRET as string,
+            issuer: process.env.AUTH0_ISSUER as string,
+        }),
+        Facebook({
+            clientId: process.env.FACEBOOK_CLIENT_ID as string,
+            clientSecret: process.env.FACEBOOK_CLIENT_SECRET as string,
+        }),
+        Github({
+            clientId: process.env.GITHUB_CLIENT_ID as string,
+            clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
+        }),
+        Google({
+            clientId: process.env.GOOGLE_CLIENT_ID as string,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+        }),
+        Twitter({
+            clientId: process.env.TWITTER_CLIENT_ID as string,
+            clientSecret: process.env.TWITTER_CLIENT_SECRET as string,
+            version: process.env.TWITTER_VERSION as string,
+        }),
+        LinkedIn({
+            clientId: process.env.LINKEDIN_CLIENT_ID as string,
+            clientSecret: process.env.LINKEDIN__CLIENT_SECRET as string,            
+        }),
     ],
     secret: process.env.SECRET as string,
     callbacks: {
+        
         async signIn({ user, account, profile, email, credentials }: any ) {          
             const isAllowedToSignIn = true
             if (isAllowedToSignIn) {
@@ -63,8 +96,8 @@ export const authOptions = {
             } 
             else {
                 console.log("invoking refresh token core!!!!");
-                const url = `${process.env.OKTA_OAUTH2_ISSUER}/v1/token`;
-                const body = `grant_type=refresh_token&client_id=${process.env.OKTA_OAUTH2_CLIENT_ID}&client_secret=${process.env.OKTA_OAUTH2_CLIENT_SECRET}&refresh_token=${token.refreshToken}`;
+                const url = `${process.env.OKTA_ISSUER}/v1/token`;
+                const body = `grant_type=refresh_token&client_id=${process.env.OKTA_CLIENT_ID}&client_secret=${process.env.OKTA_CLIENT_SECRET}&refresh_token=${token.refreshToken}`;
                 const headers = {
                     'Content-Type': 'application/x-www-form-urlencoded',
                     'Accept': 'application/json'
